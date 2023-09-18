d8820e1b2b8b4c959bd67bf56f3c4839be3e3fd0
Nameless0Null <johnlu0410@gmail.com>
2023-09-18 22:19:52 +0900

    hw1_addUser, deleteUser 기능 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -9,4 +10,34 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/:id')
+  getUsernameByIndex(@Param('id') id : number, @Res() res: Response){
+    try {
+      const username = this.appService.getUsernameByIndex(id);
+      res.send(username)
+    } catch(error) {
+      res.status(400).send(error.message);
+    }
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id : number, @Res() res : Response){
+    try {
+      this.appService.deleteUser(id);
+    } catch(error) {
+      res.status(400).send(error.message)
+    }
+  }
+  
+  @Get('/adduser/id')
+  addUser(@Param('user') user : string, @Res() res : Response) {
+    try {
+      this.appService.addUser(user);
+    } catch(error) {
+      res.status(400).send(error.message)
+    }
+  }
+
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,26 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  usernames: string[];
+  constructor(){
+    this.usernames = ['a', 'b'];
+  }
   getHello(): string {
     return 'Hello World!';
   }
+
+  getUsernameByIndex(index: number) {
+    if(index < 0 || index >= this.usernames.length) throw new Error('Invalid index');
+    return this.usernames[index];
+  }
+
+  addUser(username: string) {
+    this.usernames.push(username);
+  }
+
+  deleteUser(index: number) {
+    if(index < 0 || index >= this.usernames.length) throw new Error('Invalid index');
+    this.usernames.splice(index, 1)
+  }
+
 }
